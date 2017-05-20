9ed5bdc7512d3250d7d0c5aa1cdc6536be1ca0b7
Aaron Knister <aaron.knister@gmail.com>
2017-05-20 18:15:40 -0400

    correct num of args to setxattr func in __acl_set_file declaration

--- libacl/__acl_set_file.c
+++ libacl/__acl_set_file.c
@@ -35,7 +35,7 @@
 int
 __acl_set_file(const char *path_p, acl_type_t type, acl_t acl,
    	       ssize_t (*setxattr_fun)(const char *, const char *,
-				   void *, size_t))
+				   void *, size_t, int))
 {
 	acl_obj *acl_obj_p = ext2int(acl, acl);
 	char *ext_acl_p;
